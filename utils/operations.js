249c52e0eaf85f35b15737fa4f85569e9c78fa60
jawwad183 <mjwdmufti@gmail.com>
2026-03-31 21:02:14 +0300

    Testing autograder

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,23 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  if (numbers.length === 0) return 0;
+  return numbers.slice(1).reduce((result, num) => result - num, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((product, num) => product * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
-}
+  if (numbers.length === 0) return 0;
 
+  return numbers.slice(1).reduce((result, num) => {
+    if (num === 0) {
+      return NaN;
+    }
+    return result / num;
+  }, numbers[0]);
+}
